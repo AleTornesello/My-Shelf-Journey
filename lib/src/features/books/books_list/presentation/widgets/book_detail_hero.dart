2d0353e86f39c228950c69039c7bbfdacbd65369
AleTornesello <alessandro.tornesello99@gmail.com>
2024-05-26 15:54:00 +0200

    fix: manage image loading error in book detail image

--- lib/src/features/books/books_list/presentation/widgets/book_detail_hero.dart
+++ lib/src/features/books/books_list/presentation/widgets/book_detail_hero.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:my_shelf_journey_mobile/src/core/constants/colors.dart';
 import 'package:my_shelf_journey_mobile/src/core/constants/styles.dart';
 import 'package:my_shelf_journey_mobile/src/features/books/books_list/domain/models/book_model.dart';
 
@@ -23,6 +24,14 @@ class BookDetailHero extends StatelessWidget {
             width: 100,
             height: 150,
             fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) => Container(
+              width: 100,
+              height: 150,
+              decoration: const BoxDecoration(
+                color: ColorConstants.gray200,
+              ),
+              child: const Icon(Icons.image),
+            ),
           ),
         ),
         const Gap(StylesConstants.gap),
